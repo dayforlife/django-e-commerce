111b7169b5d0152ed1a6a43eb69accc6a9b3a5b1
dayforlife <eldiyar_alymbekov@icloud.com>
2025-01-12 05:22:35 +0600

    create store.html and do urls

--- core/urls.py
+++ core/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
 from core.views import home
@@ -24,4 +24,5 @@ from core.views import home
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
+    path('store/', include('store.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
