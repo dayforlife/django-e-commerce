cdc2d69548de0dd52d79a377109777481aaa9a66
dayforlife <eldiyar_alymbekov@icloud.com>
2025-01-13 09:00:08 +0600

    done: login logic and logout

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
-from django.contrib import messages
+from django.contrib import messages, auth
 from django.shortcuts import redirect, render
+
+from django.contrib.auth.decorators import login_required
 
 from accounts.forms import RegistrationForm
 from accounts.models import Account
@@ -28,8 +30,26 @@ def register(request):
     }
     return render(request, 'accounts/register.html', context)
 
+
 def login(request):
+    if request.method == 'POST':
+        email       = request.POST['email']
+        password    = request.POST['password']
+        
+        user = auth.authenticate(email=email, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            # messages.success(request, 'You are now logged in.')
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid login credentials')
+            return redirect('login')
     return render(request, 'accounts/login.html')
 
+
+@login_required(login_url = 'login')
 def logout(request):
-    return 
+    auth.logout(request)
+    messages.success(request, 'You are logged out.')
+    return redirect('login')
