8de554fdaa3665e4f2bd65d8f4bab7bb7072d4d8
dayforlife <eldiyar_alymbekov@icloud.com>
2025-01-15 08:43:11 +0600

    create Orders app models place_order func and forms

--- core/urls.py
+++ core/urls.py
@@ -26,5 +26,7 @@ urlpatterns = [
     path('', home, name='home'),
     path('store/', include('store.urls')),
     path('cart/', include('carts.urls')),
-    path('accounts/', include('accounts.urls'))
+    path('accounts/', include('accounts.urls')),
+
+    path('orders/', include('orders.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
